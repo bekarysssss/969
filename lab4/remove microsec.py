30ad48c1ab6347ba3f94e7eec9b328d2d0865c53
bekarysssss <156634442+bekarysssss@users.noreply.github.com>
2024-02-18 00:12:42 +0600

    Delete lab4/remove microsec.py

--- lab4/remove microsec.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-current_time = datetime.now()
-
-without_microseconds = current_time.replace(microsecond=0)
-
-print("Current time:", current_time)
-print("Without microseconds:", without_microseconds)
