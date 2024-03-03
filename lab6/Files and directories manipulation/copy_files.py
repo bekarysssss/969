c49a94cb8d2669bd08a5f622e62b88a95e3f19a7
bekarysssss <156634442+bekarysssss@users.noreply.github.com>
2024-03-04 00:58:55 +0500

    Delete lab6/Files and directories manipulation/copy_files.py

--- lab6/Files and directories manipulation/copy_files.py
+++
@@ -1,7 +0,0 @@
-import os
-
-path1 = input("Vvedite put' k 1 failam:  ")
-path2 = input("Vvedite put' k 2 failam:  ")
-with open(path1,'r') as first, open(path2,'a') as second:
-    for x in first:
-        second.write(x)
